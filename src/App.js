587bd526c12dc3b9ff69ff1ccc78cd7b7b21d6a2
fallynlogan <falo7122@colorado.edu>
2025-04-15 15:05:42 -0400

    feat: add handler for button press

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function call_rest() {
-  
+
+}
+
+function TextArea({value}) {
+  return (
+    <div className='text-area'>
+      value
+    </div>
+  );
+}
+
+function Button() {
+  function handleClick() {
+    console.log('clicked');
+  }
+
+  return (
+    <button 
+      className='button'
+      onClick={handleClick}
+    >
+      Click Me
+    </button>
+  );
 }
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        text
+    <>
+      <div className="Main">
+        <TextArea />
+        <Button />
       </div>
-      <button>
-        Click Me
-      </button>
-    </div>
+    </>
   );
 }
 
